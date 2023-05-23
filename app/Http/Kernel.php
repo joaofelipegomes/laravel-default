c8645daa370902466bba83340142693031393c25
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-23 20:33:42 -0300

    Initial commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,9 +45,12 @@ class Kernel extends HttpKernel
     ],
 
     'admin' => [
-      // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-      \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
-      \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        \App\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\VerifyCsrfToken::class,
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
     ],
   ];
 
