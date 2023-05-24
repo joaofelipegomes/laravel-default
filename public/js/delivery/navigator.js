50318dda183781e80347d427edc1a53c80fc5a2f
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-24 16:53:26 -0300

    Delivery: Navigator JS

--- public/js/delivery/navigator.js
+++ public/js/delivery/navigator.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
-  document.querySelector('select[name="categories-dropdown"]').addEventListener('change', function() {
-    const section = document.querySelector('select[name="categories-dropdown"]').value
-    alert(section)
-    document.getElementById(section).scrollIntoView();
-  })
+  document.querySelectorAll('select[name="categories-dropdown"]').forEach(element => {
+    element.addEventListener('change', function() {
+      const section = document.querySelector('select[name="categories-dropdown"]').value
+      alert(section)
+      document.getElementById(section).scrollIntoView();
+    })
+  });
 })
