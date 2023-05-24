e9c30cf84dadfee15d79e7f6e33d9ec4fb9cb3ac
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-24 11:16:04 -0300

    Delivery Route

--- public/js/delivery/app.js
+++ public/js/delivery/app.js
@@ -205,7 +205,7 @@ async function init() {
 
     async function storeStatus(store) {
         return new Promise((resolve, reject) => {
-            fetch(`http://inovadelivery-env.eba-pibcpwue.sa-east-1.elasticbeanstalk.com/store/1/status`, {
+            fetch(`http://inovadelivery-env.eba-pibcpwue.sa-east-1.elasticbeanstalk.com/delivery/store/1/status`, {
                 method: 'GET',
                 headers: {
                     "Content-Type": "application/json",
