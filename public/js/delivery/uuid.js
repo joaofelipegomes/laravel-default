9c2cc0397a98acf218a44b8e16b714972933c916
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-24 12:05:35 -0300

    Delivery Route

--- public/js/delivery/uuid.js
+++
@@ -1,2 +0,0 @@
-import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
-console.log(uuidv4()); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
--- resources/views/delivery/_templates/default/index.blade.php
+++ resources/views/delivery/_templates/default/index.blade.php
@@ -8,10 +8,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.11.1/dist/cdn.min.js"></script>
-    <script src="https://unpkg.com/swup@3"></script>
     <link href="{{ asset('css/delivery/dist.css') }}" rel="stylesheet">
     <script type="module" src="{{ asset('js/delivery/app.js') }}"></script>
-    <script type="module" src="{{ asset('js/delivery/uuid.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/date-and-time@3.0.0/date-and-time.min.js"></script>
     @yield('header')
 </head>
