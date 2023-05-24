5ace9b7b90e6851021562f88151a237702332451
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-23 22:54:03 -0300

    Initial commit

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -12,8 +12,6 @@ class EncryptCookies extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        protected $except = [
-            'userInovaAdmin'
-          ];
+        'userInovaAdmin'
     ];
 }
