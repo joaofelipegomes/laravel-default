a876c3a5fbd26f9261b5d5141664d2462763481d
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-24 20:27:58 -0300

    Force Scheme HTTPS

--- resources/views/delivery/_templates/default/index.blade.php
+++ resources/views/delivery/_templates/default/index.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>@yield('title') • Cardápio e Delivery em Campinas</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('img/icon_delivery.png') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
