513a13f0576d12363944cd205d9ff1709b20e481
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-24 08:40:21 -0300

    Delivery Route

--- routes/delivery.php
+++ routes/delivery.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Delivery\HomeController;
+use App\Http\Controllers\Delivery\ProductController;
+use App\Http\Controllers\Delivery\CartController;
+use App\Http\Controllers\Delivery\ProfileController;
 
 Route::get(
-  '/check/{document}',
+  '/',
   function () {
     return 'oi';
   }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./resources/views/admin/**/*.{js,php}",
+      "./resources/views/admin/**/**/*.{js,php}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
