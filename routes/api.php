4cd5dd173ec5d0dbc334954565d2dab314ea76fd
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-23 22:35:13 -0300

    Initial commit

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\API\APIController;
+use App\Http\Controllers\Admin\APIController;
 use App\Http\Controllers\Admin\MailController;
 use App\Services\KeyRequestService;
 
