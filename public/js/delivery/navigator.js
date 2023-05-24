00cbfa78bb350253684432aa6001fe53c5d28fe4
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-24 16:50:33 -0300

    Delivery: Navigator JS

--- public/js/delivery/navigator.js
+++ public/js/delivery/navigator.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   document.querySelector('select[name="categories-dropdown"]').addEventListener('change', function() {
     const section = document.querySelector('select[name="categories-dropdown"]').value
-    window.scrollTo(section.offsetLeft, section.offsetTop);
+    document.getElementById(section).scrollIntoView();
   })
 })
