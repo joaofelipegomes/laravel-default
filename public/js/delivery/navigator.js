84ad8d6c4374bc5eff100a39251c91b5380a6ee3
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-25 10:10:20 -0300

    Delivery navigator JS

--- public/js/delivery/navigator.js
+++ public/js/delivery/navigator.js
@@ -1,16 +1,21 @@
 $(document).ready(function () {
   document.querySelector('select[name="categories-dropdown-mask"]').addEventListener('change', function() {
     document.querySelector('select[name="categories-dropdown"]').value = document.querySelector('select[name="categories-dropdown-mask"]').value
+    scrollSection(document.querySelector('select[name="categories-dropdown-mask"]').value)
   })
 
   document.querySelector('select[name="categories-dropdown"]').addEventListener('change', function() {
     const section = document.querySelector('select[name="categories-dropdown"]').value
-    var { top } = getOffset(document.getElementById(section))
-    var { left } = 0
-
-    window.scrollTo({ left: 0, top: top - 125, behavior: 'smooth' })
+    scrollSection(section)
   })
 })
+
+const scrollSection = (section) => {
+  var { top } = getOffset(document.getElementById(section))
+  var { left } = 0
+
+  window.scrollTo({ left: 0, top: top - 125, behavior: 'smooth' })
+}
 
 function getOffset( el ) {
   var _x = 0;
