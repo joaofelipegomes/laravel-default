eb155266da99a902513dde838fdd6ae7348d431f
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-25 09:52:31 -0300

    Delivery & Manual

--- public/js/delivery/navigator.js
+++ public/js/delivery/navigator.js
@@ -2,7 +2,14 @@ $(document).ready(function () {
   document.querySelectorAll('select[name="categories-dropdown"]').forEach(element => {
     element.addEventListener('change', function() {
       const section = document.querySelector('select[name="categories-dropdown"]').value
-      document.getElementById(section).scrollIntoView()
+      var { top } = getOffset(document.getElementById(section))
+      var { left } = getOffset(0)
+
+      window.scrollTo({ left: 0, top: top - 125, behavior: 'smooth' })
+
+      // document.getElementById(section).scrollIntoView()
     })
   });
 })
+
+
--- routes/help.php
+++ routes/help.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Help\HomeController;
 
-Route::get('/', function () {
-  return redirect('https://www.inovasistemas.com.br');
-});
-
 Route::get(
   '/',
   [HomeController::class, 'home']
