4d070a7bcc7c6a706292f8adbe421f6735e677c9
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-25 11:57:02 -0300

    Delivery JS Files

--- public/js/delivery/dist.js
+++ public/js/delivery/dist.js
@@ -34,22 +34,6 @@ const storeURI = () => {
   return routeParametersRegex[1]
 }
 
-const verifyStoreStatus = () => {
-  const status = document.querySelector('.info > .open')
-  status.setAttribute('class', '')
-
-  if ((!data.open) && (!data.outside_business_hours)) {
-    status.setAttribute('class', 'open closed')
-    status.querySelector('.status').innerHTML = 'Loja Fechada'
-  } else if ((!data.open) && (data.outside_business_hours)) {
-    status.setAttribute('class', 'open schedule')
-    status.querySelector('.status').innerHTML = 'Agendar Entrega'
-  } else if (data.open) {
-    status.setAttribute('class', 'open opened')
-    status.querySelector('.status').innerHTML = 'Loja Aberta'
-  }
-}
-
 const navigatorWeight = (uri) => {
   const menus = document.querySelectorAll('.active')
   menus.forEach(menu => {
@@ -81,21 +65,6 @@ const displayView = () => {
   $('body').delay(500).animate({
     opacity: 1
   }, 300)
-}
-
-const storeStatus = async () => {
-  return new Promise((resolve, reject) => {
-    fetch(`https://api.inovasistemas.app/delivery/store/1/status`, {
-      method: 'GET',
-      headers: {
-        "Content-Type": "application/json",
-      }
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        return resolve(data);
-      })
-  })
 }
 
 const navigator = async () => {
--- public/js/delivery/home.js
+++ public/js/delivery/home.js
@@ -1,3 +1,35 @@
+const storeStatus = async () => {
+  return new Promise((resolve, reject) => {
+    fetch(`https://api.inovasistemas.app/delivery/store/1/status`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        return resolve(data);
+      })
+  })
+}
+
+const verifyStoreStatus = async () => {
+  const data = await storeStatus()
+  const status = document.querySelector('.info > .open')
+  status.setAttribute('class', '')
+
+  if ((!data.open) && (!data.outside_business_hours)) {
+    status.setAttribute('class', 'open closed')
+    status.querySelector('.status').innerHTML = 'Loja Fechada'
+  } else if ((!data.open) && (data.outside_business_hours)) {
+    status.setAttribute('class', 'open schedule')
+    status.querySelector('.status').innerHTML = 'Agendar Entrega'
+  } else if (data.open) {
+    status.setAttribute('class', 'open opened')
+    status.querySelector('.status').innerHTML = 'Loja Aberta'
+  }
+}
+
 (async () => {
-  await storeStauts()
+  await verifyStoreStatus()
 })()
