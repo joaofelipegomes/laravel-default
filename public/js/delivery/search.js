8e628fc18415007047a0155b48eb435f3c1914f7
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-26 12:33:41 -0300

    Delivery Search

--- public/js/delivery/search.js
+++ public/js/delivery/search.js
@@ -58,7 +58,7 @@ const createElements = (data, element) => {
       product.querySelector('.description').innerHTML = data[object]['description']
       product.querySelector('.price').innerHTML = data[object]['amount']
 
-      product.querySelector('.image').style.backgroundImage = `https://solucoesinova.com.br/inovadelivery/produto_img/${data[object]['image']}`
+      product.querySelector('.image').style.backgroundImage = `url('https://solucoesinova.com.br/inovadelivery/produto_img/${data[object]['image']}')`
 
       container.appendChild(product)
     }
