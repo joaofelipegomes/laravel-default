60378bdad5063b9e6bddeddb50d4fa94f9d6c9c2
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-26 18:34:49 -0300

    Support Route

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,7 +53,7 @@ class Kernel extends HttpKernel
       \Illuminate\Routing\Middleware\SubstituteBindings::class,
     ],
 
-    'os' => [
+    'suporte' => [
       \App\Http\Middleware\EncryptCookies::class,
       \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
       \Illuminate\Session\Middleware\StartSession::class,
