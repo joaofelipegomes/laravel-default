6f55fc1264a64d1c710eb0fc301c5ded07926d24
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-26 09:13:57 -0300

    Delivery Search

--- resources/views/delivery/search/index.blade.php
+++ resources/views/delivery/search/index.blade.php
@@ -4,7 +4,7 @@
 @section("body")
 <div id="swup" class="main-container no-padding swup-transition-fade">
   <div class="item-container">
-    <div class="back-container">
+    <div class="back-container" style="width: auto; padding-right: 0px;">
       <a href="/delivery/{{ getCookies('store') }}" class="back-navigator">
         <span class="material-symbols-outlined weight">arrow_back_ios</span>
       </a>
@@ -16,7 +16,7 @@
       <div class="search-container">
         <div class="search">
           <span class="material-symbols-outlined">search</span>
-          <input type="text" name="search" value="" placeholder="Digite sua buscas">
+          <input type="text" name="search" value="" placeholder="Digite sua busca">
         </div>
       </div>
     </div>
