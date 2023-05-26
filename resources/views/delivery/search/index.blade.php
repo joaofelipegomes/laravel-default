82b629188dda6eb5ce39dc0a1b9b2876404ba2a7
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-26 08:58:44 -0300

    Delivery Search

--- resources/views/delivery/search/index.blade.php
+++ resources/views/delivery/search/index.blade.php
@@ -3,28 +3,26 @@
 
 @section("body")
 <div id="swup" class="main-container no-padding swup-transition-fade">
-    <div class="item-container">
-        <div class="back-container">
-            <a href="/delivery/{{ getCookies('store') }}" class="back-navigator">
-                <span class="material-symbols-outlined weight">
-                    arrow_back_ios
-                </span>
-            </a>
-        </div>
-      </div>
+  <div class="item-container">
+    <div class="back-container">
+      <a href="/delivery/{{ getCookies('store') }}" class="back-navigator">
+        <span class="material-symbols-outlined weight">arrow_back_ios</span>
+      </a>
+    </div>
+  </div>
 
-        <div class="search-details">
-          <div class="group-search">
-            <div class="search-container">
-                <a href="/delivery/flordavila/buscar" class="search">
-                    <span class="material-symbols-outlined">
-                        search
-                    </span>
-                    <p>Buscar na Flor da Vila Mini Salgados</p>
-                </a>
-            </div>
+  <div class="content-container">
+    <div class="search-details">
+      <div class="group-search">
+        <div class="search-container">
+          <a href="/delivery/flordavila/buscar" class="search">
+            <span class="material-symbols-outlined">search</span>
+            <p>Buscar na Flor da Vila Mini Salgados</p>
+          </a>
         </div>
       </div>
+    </div>
+  </div>
 </div>
 @stop
 
