f04f8cb63dba4e45a797411044095fab917e7826
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-26 19:08:03 -0300

    Support Route

--- routes/support.php
+++ routes/support.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\EnsureUserIsValidOS;
 use App\Http\Controllers\Support\AuthController;
 
 Route::middleware(EnsureUserIsValidOS::class)->get(
