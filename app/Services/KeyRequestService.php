32ce6a79046af0d97ae88c2a8be374197e15af52
Joao Gomes <contato@joaofelipegomes.com.br>
2023-05-30 14:10:11 -0300

    Postback test

--- app/Services/KeyRequestService.php
+++ app/Services/KeyRequestService.php
@@ -6,6 +6,6 @@ class KeyRequestService extends BaseRequestServiceKey
 {
   public function key($document_number, $trade_name, $due_date)
   {
-    return $this->get(sprintf("%s/%s?%s=%s&%s=%s&%s=%s", "chave", "set", "document_number", $document_number, 'trade_name', $trade_name, 'due_date', $due_date));
+    return sprintf("%s/%s?%s=%s&%s=%s&%s=%s", "chave", "set", "document_number", $document_number, 'trade_name', $trade_name, 'due_date', $due_date);
   }
 }
