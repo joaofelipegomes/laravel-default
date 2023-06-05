8f62c0bcb49c33a93b099946b50edbb394372c30
Joao Gomes <contato@joaofelipegomes.com.br>
2023-06-05 13:39:59 -0300

    Main Route

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CheckController;
 
 Route::get('/', function () {
-  return response('', 200)
-    ->header('Content-Type', 'text/plain');
+  return response('', 200)->header('Location', 'http://inovasistemas.com.br/');
+  /* return response('', 200)
+    ->redirectTo('Content-Type', 'text/plain'); */
 });
 
 Route::get(
